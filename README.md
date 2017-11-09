b811ec40290214cf1390923473c22ebafaaa5cd3
Madi Cooley <madimaecooley@gmail.com>
2017-11-09 14:53:50 -0700

    updated README

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ News Aggregator
 ===============
 This is a website that aggregates daily news articles from multiple news sites to one place.
 This project is still in progress.
+
+
+How to run spiders:
+
+Individual Spider:
+
+1. cd /NewsAgg/spiders/spiders/spiders
+
+2. scrapy crawl npr_spider
+
+All Spiders:
+
+1. cd /NewsAgg/spiders
+
+2. python run_all.py 
--- spiders/spiders/spiders/misc_functions.py
+++ spiders/spiders/spiders/misc_functions.py
@@ -1,8 +1,8 @@
 def print_item(item):
-    # print "TITLE: %s" % item.get('Title')
-    # print "URLS: %s" % item.get('URL')
-    # print "SUMMARY: %s" % item.get('Summary')
-    # print "IMG URL: %s" % item.get('Photo')
-    # print "IMG CAPTION: %s" % item.get('Credit_Caption')
+    print "TITLE: %s" % item.get('Title')
+    print "URLS: %s" % item.get('URL')
+    print "SUMMARY: %s" % item.get('Summary')
+    print "IMG URL: %s" % item.get('Photo')
+    print "IMG CAPTION: %s" % item.get('Credit_Caption')
     print "ARTICLE SITE: %s" % item.get('Site')
     print
